222c5b2d9f568309c2ef1db3734679bccc407cd8
Rohit Nandi <rohitnandibmp@gmail.com>
2023-10-24 14:58:34 +0530

    after model creation

--- server.js
+++ server.js
@@ -1,6 +1,26 @@
 import { ApolloServer } from "apollo-server";
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
 import typeDefs from "./schemaGql.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("Connected to the db");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log("DB connection error", err);
+});
+
+import "./models/Quotes.js";
+import "./models/User.js";
 import resolvers from "./resolvers.js";
 
 const server = new ApolloServer({
