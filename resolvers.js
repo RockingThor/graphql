9e26eab21e45d3e98fe37687431a5813387a13f6
Rohit Nandi <rohitnandibmp@gmail.com>
2023-10-24 18:32:17 +0530

    after signin and signup

--- resolvers.js
+++ resolvers.js
@@ -1,5 +1,10 @@
 import { quotes, users } from "./fakedb.js";
 import { randomBytes } from "crypto";
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import Jwt from "jsonwebtoken";
+
+const User = mongoose.model("User");
 
 const resolvers = {
   Query: {
@@ -12,24 +17,45 @@ const resolvers = {
     quotes: () => {
       return quotes;
     },
-    user: (_, { id }) => {
-      return users.find((user) => user.id == id);
+    user: (_, { _id }) => {
+      return users.find((user) => user._id == _id);
     },
     quote: (_, { by }) => {
       return quotes.filter((quote) => quote.by === by);
     },
   },
   User: {
-    quotes: (ur) => quotes.filter((quote) => quote.by === ur.id),
+    quotes: (ur) => quotes.filter((quote) => quote.by === ur._id),
   },
   Mutation: {
-    createUserDummy: (_, { userNew }) => {
-      const id = randomBytes(5).toString("hex");
-      users.push({
-        id,
+    signupUser: async (_, { userNew }) => {
+      const user = await User.findOne({ email: userNew.email });
+      if (user) {
+        throw new Error("User already exist with this email");
+      }
+      const hashedPassword = await bcrypt.hash(userNew.password, 12);
+      const newUser = await new User({
         ...userNew,
+        password: hashedPassword,
       });
-      return users.find((user) => user.id === id);
+      return await newUser.save();
+    },
+    signinUser: async (_, { userData }) => {
+      const user = await User.findOne({ email: userData.email });
+      if (user) {
+        const passMatch = await bcrypt.compare(
+          userData.password,
+          user.password
+        );
+        if (passMatch) {
+          const token = Jwt.sign({ userId: user._id }, process.env.JWT_KEY);
+          return { token };
+        } else {
+          throw new Error("Email or Password is invalid");
+        }
+      } else {
+        throw new Error("No user found with the specified email");
+      }
     },
   },
 };
--- schemaGql.js
+++ schemaGql.js
@@ -4,12 +4,12 @@ const typeDefs = gql`
   type Query {
     greet: String
     users: [User]
-    user(id: ID!): User
+    user(_id: ID!): User
     quotes: [Quote]
     quote(by: String!): [Quote]
   }
   type User {
-    id: ID
+    _id: ID
     firstName: String
     lastName: String
     email: String
@@ -19,12 +19,20 @@ const typeDefs = gql`
     name: String
     by: String
   }
+  type Token {
+    token: String
+  }
   type Mutation {
-    createUserDummy(userNew: UserInput): User
+    signupUser(userNew: UserInput!): User
+    signinUser(userData: UserSigninInput!): Token
   }
   input UserInput {
     firstName: String!
     lastName: String!
+    email: String!
+    password: String!
+  }
+  input UserSigninInput {
     email: String!
     password: String!
   }
